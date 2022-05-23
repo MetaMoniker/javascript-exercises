274cff47fb79b4f105888f5d9b2becf3f72bfc25
MetaMoniker <metamoniker@gmail.com>
2022-05-23 17:56:54 -0400

    finished 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+let words = 'hello';
+
+const reverseString = function (words) {
+    let strArr = [];
+
+    // put words in array
+    for (i = 0; i < words.length; i++) {
+        strArr.push(words[i]);
+    }
+
+    //reverse array and to string
+    let output1 = strArr.reverse().join('');
+    return output1;
+
 
 };
 

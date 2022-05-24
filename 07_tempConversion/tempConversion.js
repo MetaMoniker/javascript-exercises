61393807548a3d080e0e47ade542765f1ded6575
MetaMoniker <metamoniker@gmail.com>
2022-05-23 21:42:59 -0400

    used toFixed rather than Math.Round (oops)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const ftoc = function() {
+const ftoc = function (ftemp) {
+
+  // C = (F - 32) * (5/9)
+  let ctemp = (ftemp - 32) * (5 / 9);
+  let c = parseFloat(ctemp.toFixed(1));
+
+  console.log(c);
+  return c;
 
 };
 
-const ctof = function() {
+const ctof = function (ctemp) {
+
+  // F = C * ( 9 / 5 ) + 32
+  let ftemp = ctemp * (9 / 5) + 32;
+  let f = parseFloat(ftemp.toFixed(1));
+
+  console.log(f);
+  return f;
+
 
 };
 

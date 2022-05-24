9794b674491aa4d5d52d71f1b57704d722d0da9d
MetaMoniker <metamoniker@gmail.com>
2022-05-23 21:07:30 -0400

    used remainder in conditionals

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    console.log('1900 % 400 : ', 1900 % 400);
+    console.log('1900 % 100 : ', 1900 % 100);
+
+
+
+    if (year % 400 != 0 && year % 100 == 0) {
+        return false;
+    } else {
+        if (year % 4 == 0) {
+            return true;
+        } else if (year % 4 != 0) {
+            return false;
+        }
+    }
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
--- 06_leapYears/leapYears.spec.js
+++ 06_leapYears/leapYears.spec.js
@@ -1,22 +1,22 @@
 const leapYears = require('./leapYears')
 
 describe('leapYears', () => {
-  test('works with non century years', () => {
+  test('1 works with non century years', () => {
     expect(leapYears(1996)).toBe(true);
   });
-  test.skip('works with non century years', () => {
+  test('2 works with non century years', () => {
     expect(leapYears(1997)).toBe(false);
   });
-  test.skip('works with ridiculously futuristic non century years', () => {
+  test('3 works with ridiculously futuristic non century years', () => {
     expect(leapYears(34992)).toBe(true);
   });
-  test.skip('works with century years', () => {
+  test('4 works with century years', () => {
     expect(leapYears(1900)).toBe(false);
   });
-  test.skip('works with century years', () => {
+  test('5 works with century years', () => {
     expect(leapYears(1600)).toBe(true);
   });
-  test.skip('works with century years', () => {
+  test('6 works with century years', () => {
     expect(leapYears(700)).toBe(false);
   });
 });
